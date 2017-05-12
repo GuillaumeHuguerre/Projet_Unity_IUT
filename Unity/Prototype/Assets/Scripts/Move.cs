3f9e6f3896f6f110848b4480f37632fd969ebdc7
Guillaume HUGUERRE <guillaume.huguerre@orange.fr>
2017-05-12 18:45:50 +0200

    Add Arthur's files

---
+++ Unity/Prototype/Assets/Scripts/Move.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Move : MonoBehaviour
+{
+
+    public float MAX_SPEED;
+    public float acceleration;
+    private float speed;
+    private SpriteRenderer p_sr;
+    Animator _anim;
+
+    private bool pousseeEnCours;
+    private bool pousseInverse=false;
+
+    private float climbSpeed;
+    public bool isClimber;
+
+    void Awake()
+    {
+        if (MAX_SPEED <= 0) MAX_SPEED = 2;
+        if (acceleration <= 0) acceleration = 0.5f;
+    }
+
+    void Start()
+    {
+        climbSpeed = constantes.CLIMBSPEED;
+        speed = constantes.SPEED;
+
+        p_sr = this.GetComponent<SpriteRenderer>();
+        _anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            speed += acceleration;
+            if (speed > MAX_SPEED) speed = MAX_SPEED;
+            _doMove();
+            return;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            speed -= acceleration;
+            if (speed < -MAX_SPEED) speed = -MAX_SPEED;
+            _doMove();
+            return;
+        }
+
+        if (speed != 0)
+        {
+            // aucune touche presséé : deceleration
+            speed = speed + Mathf.Sign(speed) * -acceleration;
+            _doMove();
+        }
+        if (Mathf.Abs(speed) < acceleration)
+            // vitesse insignifiante => =0
+            speed = 0.0f;
+    }
+
+    void _doMove()
+    {
+        _anim.SetFloat("Speed", Mathf.Abs(speed));
+        if (speed != 0f) p_sr.flipX = (speed < 0);
+        if (!pousseInverse)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector3.right * -speed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Wall" && isClimber == true)
+        {
+            transform.Translate(Vector3.up * climbSpeed * Time.deltaTime);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, climbSpeed);
+        }
+    }
+}
