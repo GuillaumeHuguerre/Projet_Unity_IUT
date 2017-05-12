a1811693f68ec6f94f82450a15458ad8b3f63968
Guillaume HUGUERRE <guillaume.huguerre@orange.fr>
2017-05-12 20:32:11 +0200

    Revert "update"
    This reverts commit 500eac488077812bc45290f57a573baa0f1b19c5.

---
+++ Unity/Prototype/Assets/End.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class End : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+            Destroy(other.gameObject);
+    }
+}
