91eb095ee2fbc33bfeb265860ef09f022de8c59b
nivignal <nicolas.vignal@etu.udamail.fr>
2017-05-24 18:14:46 +0200

    a

--- Prototype/Assets/Scripts/Variables.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class variables
-{
-    static public bool doorIsActivated; //montre si le bouton est activé
-    static public bool boxIsPushed; //montre si le bouton est activé
-}
