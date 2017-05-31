3311d0fcae6f6fbd6dace335d7423b56ecf8acac
Guillaume HUGUERRE <guillaume.huguerre@orange.fr>
2017-05-31 09:21:00 +0200

    Update

---
+++ Projet-Unity/Prototype/Assets/Scripts/Score.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    private int nbrVivant;
+    private int nbrSauves;
+
+    public Text sauvesText;
+    public Text vivantText;
+
+    void Awake()
+    {
+        nbrVivant = constantes.NBRVIVANT;
+        nbrSauves = constantes.NBRSAUVES;
+        setSauvesText();
+        setVivantText();
+    }
+
+    void Death()
+    {
+        if (nbrVivant > 0)
+        {
+            --nbrVivant;
+            setVivantText();
+        }
+        else
+            print("Vous avez perdu !");
+    }
+
+    void Save()
+    {
+        ++nbrSauves;
+        setSauvesText();
+        if (nbrVivant > 0)
+        {
+            --nbrVivant;
+            setVivantText();
+        }
+        else
+            print("Niveau Terminé !");
+    }
+
+    void setVivantText()
+    {
+        vivantText.text = "Vivants : " + nbrVivant.ToString();
+    }
+
+    void setSauvesText()
+    {
+        sauvesText.text = "Sauvés : " + nbrSauves.ToString();
+    }
+}
