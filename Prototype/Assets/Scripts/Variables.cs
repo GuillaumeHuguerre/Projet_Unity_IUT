d2315285ca16d0668c0b857ba6c35843312ea11b
Guillaume HUGUERRE <guillaume.huguerre@orange.fr>
2017-05-31 08:58:48 +0200

    Update

--- Prototype/Assets/Scripts/Variables.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class variables
-{
-    static public bool doorIsActivated; //montre si le bouton est activé
-}
