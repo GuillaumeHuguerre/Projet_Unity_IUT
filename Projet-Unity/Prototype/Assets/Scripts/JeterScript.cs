592a077948fb8586474c0d1cbf542eca8907efee
nivignal <nicolas.vignal@etu.udamail.fr>
2017-06-17 16:55:58 +0200

    a

---
+++ Projet-Unity/Prototype/Assets/Scripts/JeterScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JeterScript : MonoBehaviour {
+
+    private Rigidbody2D rig;
+
+    void Awake()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        rig = this.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (this.GetComponent<Actions>().isJeter)
+        {
+            gameObject.layer = 8;
+            rig.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (this.GetComponent<Actions>().isJeter && collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(rig.velocity.x * Time.deltaTime, 10f);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
